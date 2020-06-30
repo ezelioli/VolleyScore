a545a339d38cc309dee97cfbbc5a58db836b38d2
Enrico Zelioli <enricozelioli97@gmail.com>
2020-06-30 15:01:30 +0200

    load/save teams and players

--- src/db/DBAccessManager.java
+++ src/db/DBAccessManager.java
@@ -2,6 +2,7 @@ package db;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.time.LocalDate;
 import java.util.ArrayList;
 
 import exceptions.DatabaseException;
@@ -35,13 +36,37 @@ public class DBAccessManager implements DatabaseAccess {
 	}
 
 	@Override
+	public void saveTeam(Team team, Championship championship) throws DatabaseException {
+		try{
+			String query = "INSERT INTO team VALUES (\"" + team.getName() + "\", \"" + championship.getName() + "\");";
+			db.executeUpdate(query);
+		}catch(SQLException exception){
+			throw new DatabaseException(exception.getMessage());
+		}
+	}
+
+	@Override
+	public void savePlayer(PlayerInfo playerInfo) throws DatabaseException {
+		try{
+			String query = "INSERT INTO player VALUES (\"" + playerInfo.getName() + "\", \"" + playerInfo.getSurname()
+					+ "\", \"" + playerInfo.getBirthday() + "\", \"" + playerInfo.getNumber() + "\", \"" +
+					playerInfo.getRole() + "\", \"" + playerInfo.getTeam() + "\", \"" + playerInfo.getNationality() +
+					"\");";
+			db.executeUpdate(query);
+		}catch(SQLException exception){
+			throw new DatabaseException(exception.getMessage());
+		}
+	}
+
+	@Override
 	public ArrayList<Championship> loadChampionships() throws DatabaseException {
 		ArrayList<Championship> championships = new ArrayList<Championship>();
 		try {
 			String query = "SELECT name FROM championship;";
 			ResultSet rs = db.executeQuery(query);
 			while(rs.next()){
-				championships.add(new Championship(rs.getString(1)));
+				Championship newChampionship = new Championship(rs.getString(1));
+				championships.add(newChampionship);
 			}
 		}catch(SQLException e) {
 			throw new DatabaseException(e.getMessage());
@@ -53,14 +78,38 @@ public class DBAccessManager implements DatabaseAccess {
 	public ArrayList<Team> loadTeamsOfChampionship(Championship championship) throws DatabaseException {
 		ArrayList<Team> teams = new ArrayList<Team>();
 		try {
-			String query = ""; //TODO: implement query for loading teams of a given championship from database
+			String query = "SELECT name FROM team WHERE championship = \"" + championship.getName() + "\";";
 			ResultSet rs = db.executeQuery(query);
 			while(rs.next()){
-				teams.add(new Team(rs.getString(1)));
+				Team newTeam = new Team(rs.getString(1));
+				teams.add(newTeam);
 			}
 		}catch(SQLException e) {
 			throw new DatabaseException(e.getMessage());
 		}
 		return teams;
+	}
+
+	@Override
+	public ArrayList<PlayerInfo> loadPlayersOfTeam(Team team) throws DatabaseException {
+		ArrayList<PlayerInfo> players = new ArrayList<PlayerInfo>();
+		try {
+			String query = "SELECT name, surname, birthday, nationality, number, role FROM player WHERE team = \"" +
+					team.getName() + "\";";
+			ResultSet rs = db.executeQuery(query);
+			while(rs.next()){
+				String playerName = rs.getString(1);
+				String playerSurname = rs.getString(2);
+				LocalDate playerBirthday = rs.getDate(3).toLocalDate();
+				String playerNationality = rs.getString(4);
+				int playerNumber = rs.getInt(5);
+				String playerRole = rs.getString(6);
+				PlayerInfo newPlayer = new PlayerInfo(playerName, playerSurname, playerBirthday, playerNationality, playerNumber, playerRole);
+				players.add(newPlayer);
+			}
+		}catch(SQLException e) {
+			throw new DatabaseException(e.getMessage());
+		}
+		return players;
 	}
 }
--- src/db/DatabaseAccess.java
+++ src/db/DatabaseAccess.java
@@ -6,7 +6,17 @@ import exceptions.DatabaseException;
 import domain.*;
 
 public interface DatabaseAccess {
+
 	public void saveChampionship(Championship championship) throws DatabaseException;
+
+	public void saveTeam(Team team, Championship championship) throws DatabaseException;
+
+	public void savePlayer(PlayerInfo playerInfo) throws DatabaseException;
+
 	public ArrayList<Championship> loadChampionships() throws DatabaseException;
+
 	public ArrayList<Team> loadTeamsOfChampionship(Championship championship) throws DatabaseException;
+
+	public ArrayList<PlayerInfo> loadPlayersOfTeam(Team team) throws DatabaseException;
+
 }
