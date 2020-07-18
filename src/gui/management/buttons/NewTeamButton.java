c7b7710363bc2acd2be38f76700163c16e2900f6
Enrico Zelioli <enricozelioli97@gmail.com>
2020-07-18 13:13:21 +0200

    renamed new team button

--- src/gui/management/buttons/NewTeamButton.java
+++ src/gui/management/buttons/NewTeamButton.java
@@ -4,12 +4,13 @@ import javax.swing.*;
 import java.awt.*;
 
 public class TeamsButton extends JButton {
-    private static final Color BACKGROUND = new Color(174, 176, 179);
-    private static final Color FOREGROUND = new Color(60, 63, 65);
+    private static final Color BACKGROUND = new Color(72, 155, 84);
+    private static final Color FOREGROUND = new Color(255, 255, 255);
 
     public TeamsButton(String text){
         super(text);
         setBackground(BACKGROUND);
         setForeground(FOREGROUND);
+        setFocusPainted(false);
     }
 }
